ffbb293fec4c1b0789a757207a1736e133248441
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-19 19:02:01 +0100

    Update push_swap.c

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,21 +6,37 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 21:11:09 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/10/18 14:20:17 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/10/19 14:30:21 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+
 int	main(int argc, char **argv)
 {
-	t_node stack_a;
+	Stack stack_a;
 
-	if (argc < 2)
-		exit(1);
+    // Parse command line arguments and initialize stackA.
+    if (parseArguments(argc, argv, &stack_a) != 0) {
+        return 1; // Exit with an error code.
+    }
+
+	// Parse command line arguments and initialize stacks.
+
+
+    
+    // Read input data into stackA.
+    
+    // Call your sorting function (e.g., sort).
+    
+    // Print the required operations to sort the stack.
+		ft_printf("%s", argv[0]);
+
+    // Cleanup and free memory.
 
 	
-	ft_printf("%s", argv[0]);
+
 
 	
 }

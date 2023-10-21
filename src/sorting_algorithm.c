f861d702611798b38b089b3ce28697576216cc54
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-21 18:58:36 +0100

    Update sorting_algorithm.c

--- src/sorting_algorithm.c
+++ src/sorting_algorithm.c
@@ -6,13 +6,13 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:30:28 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/10/19 14:30:43 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/10/21 12:37:47 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 // Sorting algorithm
-void sort(Stack* stackA, Stack* stackB) {
-    // Implement your sorting algorithm here.
-}
+// void sort(Stack* stackA, Stack* stackB) {
+//     // Implement your sorting algorithm here.
+// }

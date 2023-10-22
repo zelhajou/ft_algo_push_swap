af224dde2d8d427b8d2752bd73834ec2c318716e
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-22 20:55:34 +0100

    fix push function and more utils

--- src/utils.c
+++ src/utils.c
@@ -6,34 +6,84 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:33:14 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/10/21 18:44:22 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/10/22 20:40:19 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// Function to push an element onto the stack.
-void	push(t_stack **stack, int value) {
+/* Function to push an element onto the stack. */
+void	push(t_stack **stack, int value)
+{
 	t_stack	*new_node;
+	t_stack *temp;
 
 	new_node = (t_stack *)malloc(sizeof(t_stack));
+    if (new_node == NULL)
+		ft_error(0);
 	new_node->value = value;
-	new_node->next = *stack;
-	*stack = new_node;
+    new_node->next = NULL;
+	
+    if (*stack == NULL)
+    {
+        *stack = new_node;
+    }
+    else
+    {
+        temp = *stack;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
 }
 
-// Function to pop an element from the stack.
-// int pop(Stack* stack) {
-//     // Implement pop operation here.
-//     return 0; // Replace with actual implementation.
+
+void	display_list(t_stack *head)
+{
+	t_stack *temp;
+
+	temp = head;
+	if (temp == NULL)
+		ft_printf("LinkedList is empty");
+	while (temp != NULL)
+	{
+		ft_printf("%d\n", temp->value);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
+// void	display_stack(t_stack *stack_a, t_stack *stack_b)
+// {
+// 	t_stack *temp;
+// 	t_stack *temp2;
+// 	int value_a;
+// 	int value_b;
+
+// 	temp = stack_a;
+// 	temp2 = stack_b;
+// 	if (temp == NULL && temp2 == NULL)
+// 		ft_printf("stacks are empty");
+// 	while (temp != NULL && temp2 != NULL)
+// 	{
+// 		if ()
+// 		ft_printf("%d\n", temp->value);
+
+// 		temp = temp->next;
+// 	}
+// 	printf("\n");
 // }
 
-// Function to swap the top two elements of the stack.
-// void swap(Stack* stack) {
-//     // Implement swap operation here.
-// }
+void	free_stack(t_stack **stack_a)
+{
+	t_stack *temp;
 
-// Function to rotate the stack in a certain direction.
-// void rotate(Stack* stack, char direction) {
-//     // Implement rotate operation here.
-// }
+	while (*stack_a != NULL)
+	{
+		temp = *stack_a;
+		*stack_a = (*stack_a)->next;
+		free(temp);
+	}
+}

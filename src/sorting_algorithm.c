951b4c46da0b5a0d70153dfcb751a7817681a2b6
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-28 10:01:20 +0100

    Update sorting_algorithm.c

--- src/sorting_algorithm.c
+++ src/sorting_algorithm.c
@@ -11,10 +11,103 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "stack_operations.h"
 
-/*
-** Sorting algorithm
-*/
-// void sort(t_stack* stack_a, t_stack* stack_b) {
+#include "push_swap.h"
 
-// }
+
+
+void sort_three(t_stack **stack)
+{
+    if ((*stack)->value > (*stack)->next->value)
+    {
+        if ((*stack)->next->next && (*stack)->next->value > (*stack)->next->next->value)
+        {
+            ft_sa(stack);
+            ft_rra(stack);
+        }
+        else if (!(*stack)->next->next || (*stack)->value < (*stack)->next->next->value)
+        {
+            ft_sa(stack);
+        }
+        else
+        {
+            ft_ra(stack);
+        }
+    }
+    else if ((*stack)->next->next && (*stack)->value > (*stack)->next->next->value)
+    {
+        ft_rra(stack);
+    }
+}
+
+int find(t_stack *stack, int target)
+{
+	int i = 0;
+
+	while (stack != NULL)
+	{
+		if (stack->index == target)
+		{
+			return i;
+		}
+		i++;
+		stack = stack->next;
+	}
+	return (-1);
+}
+
+
+void ft_sort(t_stack **stack_a, t_stack **stack_b)
+{
+    int pv1, pv2;
+
+    if (stack_size(*stack_a) <= 3)
+    {
+        sort_three(stack_a);
+        return;
+    }
+    pv1 = 0;
+	while (stack_size(*stack_a) > 3)
+	{
+		/* code */
+	
+    pv2 = stack_size(*stack_a) / 6 + pv1;
+    pv1 = pv1 + stack_size(*stack_a) / 3;
+	printf("%d\n", pv1);
+	while (stack_size(*stack_b) <= pv1)
+	{
+
+	if ((*stack_a)->index <= pv1)
+		ft_pb(stack_a, stack_b);
+	else
+		ft_ra(stack_a);
+
+	if (*stack_b != NULL && (*stack_b)->index <= pv2)
+		ft_rb(stack_b);
+	}
+	}
+	sort_three(stack_a);
+	while (*stack_b)
+	{
+		if (find(*stack_b, (*stack_a)->index - 1) < stack_size(*stack_b) / 2)
+		{
+			while ((*stack_b)->index != (*stack_a)->index - 1)
+			{
+				ft_rb(stack_b);
+			}
+			ft_pa(stack_a, stack_b);
+		}
+		else {
+		
+			while ((*stack_b)->index != (*stack_a)->index - 1)
+			{
+				ft_rrb(stack_b);
+			}
+			ft_pa(stack_a, stack_b);
+
+		}
+	}
+
+
+}

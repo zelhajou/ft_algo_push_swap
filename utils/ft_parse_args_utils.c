ef39ea57a61d3314b663e2bce1a7fe15972fec0b
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-29 14:09:06 +0100

    add utils file for sort function

---
+++ utils/ft_parse_args_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_args_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 20:12:14 by zelhajou          #+#    #+#             */
+/*   Updated: 2023/10/29 12:45:01 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+bool	ft_is_valid_int(const char *str)
+{
+	if (*str == '-' || *str == '+')
+		str++;
+	if (!*str)
+		return (false);
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (false);
+		str++;
+	}
+	return (true);
+}
+
+bool	ft_is_within_int_range(const char *str)
+{
+	int	value;
+
+	value = ft_atoi(str);
+	if (value >= INT_MIN && value <= INT_MAX)
+		return (true);
+	return (false);
+}
+
+bool	ft_has_duplicates(t_stack *stack, int value)
+{
+	while (stack != NULL)
+	{
+		if (stack->value == value)
+			return (true);
+		stack = stack->next;
+	}
+	return (false);
+}
+
+void	ft_add_to_stack(char *token, t_stack **stack_a)
+{
+	int	value;
+
+	if (ft_is_valid_int(token) && ft_is_within_int_range(token))
+	{
+		value = ft_atoi(token);
+		if (!ft_has_duplicates(*stack_a, value))
+			ft_push(stack_a, value);
+		else
+			ft_error(1);
+	}
+	else
+		ft_error(1);
+}
+
+void	ft_handle_token(char *arg, t_stack **stack_a)
+{
+	char	**tokens;
+	int		j;
+
+	tokens = ft_split(arg, ' ');
+	if (tokens != NULL)
+	{
+		j = -1;
+		while (tokens[++j] != NULL)
+			ft_add_to_stack(tokens[j], stack_a);
+		free(tokens);
+	}
+}
--- utils/ft_sort_utils.c
+++ utils/ft_sort_utils.c
@@ -6,11 +6,22 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 18:08:22 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/10/28 22:00:37 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/10/29 12:46:49 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
+
+bool	ft_is_sorted(t_stack *stack)
+{
+	while (stack != NULL && stack->next != NULL)
+	{
+		if (stack->value > stack->next->value)
+			return (1);
+		stack = stack->next;
+	}
+	return (0);
+}
 
 void	ft_sort_top_three(t_stack **stack)
 {
@@ -46,47 +57,4 @@ int	ft_find_index(t_stack *stack, int target)
 		stack = stack->next;
 	}
 	return (-1);
-}
-
-void	ft_shift_b_to_a(t_stack **stack_a, t_stack **stack_b)
-{
-	if (ft_find_index(*stack_b, (*stack_a)->index - 1) < ft_stack_size(*stack_b) / 2)
-	{
-		while ((*stack_b)->index != (*stack_a)->index - 1)
-			ft_rb(stack_b);
-		ft_pa(stack_a, stack_b);
-	}
-	else
-	{
-		while ((*stack_b)->index != (*stack_a)->index - 1)
-			ft_rrb(stack_b);
-		ft_pa(stack_a, stack_b);
-	}
-}
-
-void	ft_shift_partition(t_stack **stack_a, t_stack **stack_b, int pv1, int pv2)
-{
-	while (ft_stack_size(*stack_b) <= pv1)
-	{
-		if ((*stack_a)->index <= pv1)
-			ft_pb(stack_a, stack_b);
-		else
-			ft_ra(stack_a);
-		if (*stack_b != NULL && (*stack_b)->index <= pv2)
-			ft_rb(stack_b);
-	}
-}
-
-void	ft_divide_and_shift(t_stack **stack_a, t_stack **stack_b)
-{
-	int	pv1;
-	int	pv2;
-
-	pv1 = 0;
-	while (ft_stack_size(*stack_a) > 3)
-	{
-		pv2 = ft_stack_size(*stack_a) / 6 + pv1;
-		pv1 += ft_stack_size(*stack_a) / 3;
-		ft_shift_partition(stack_a, stack_b, pv1, pv2);
-	}
 }

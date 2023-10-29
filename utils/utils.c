d4cbc8874b9b4614a0aca9affb16989a1e6420ef
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-29 14:07:41 +0100

    fix norm

--- utils/utils.c
+++ utils/utils.c
@@ -6,13 +6,12 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:33:14 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/10/28 20:55:23 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/10/29 12:47:12 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/* Function to push an element onto the stack. */
 void	ft_push(t_stack **stack, int value)
 {
 	t_stack	*new_node;
@@ -36,29 +35,29 @@ void	ft_push(t_stack **stack, int value)
 
 void	ft_assign_indexes(t_stack **stack_a)
 {
-    t_stack *current;
-    t_stack *tmp;
-    int index;
+	t_stack	*current;
+	t_stack	*tmp;
+	int		index;
 
-    current = *stack_a;
-    while (current)
-    {
-        tmp = *stack_a;
-        index = 0;
-        while (tmp)
-        {
-            if (tmp->value < current->value)
-                index++;
-            tmp = tmp->next;
-        }
-        current->index = index;
-        current = current->next;
-    }
+	current = *stack_a;
+	while (current)
+	{
+		tmp = *stack_a;
+		index = 0;
+		while (tmp)
+		{
+			if (tmp->value < current->value)
+				index++;
+			tmp = tmp->next;
+		}
+		current->index = index;
+		current = current->next;
+	}
 }
 
 void	ft_free_stack(t_stack **stack_a)
 {
-	t_stack *temp;
+	t_stack	*temp;
 
 	while (*stack_a != NULL)
 	{
@@ -70,17 +69,20 @@ void	ft_free_stack(t_stack **stack_a)
 
 int	ft_stack_size(t_stack *stack)
 {
-    int size = 0;
-    while (stack)
-    {
-        size++;
-        stack = stack->next;
-    }
-    return size;
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
 }
+
 void	ft_display_list(t_stack *head)
 {
-	t_stack *temp;
+	t_stack	*temp;
 
 	temp = head;
 	if (temp == NULL)

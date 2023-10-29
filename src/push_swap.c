bf7fbc77c4fe6029e88b20f52f7ae711870ed4ea
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-29 15:06:11 +0100

    update

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 21:11:09 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/10/28 21:22:30 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/10/29 13:11:09 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,11 @@ int	main(int argc, char **argv)
 	stack_a = NULL;
 	stack_b = NULL;
 	if (argc < 2)
-		ft_error(1);
+		return (0);
 	ft_parse_args(argc, argv, &stack_a);
 	ft_assign_indexes(&stack_a);
-
-
-
-	ft_printf("init stack\n");
-	ft_display_list(stack_a);
-	
 	ft_sort(&stack_a, &stack_b);
-
-	ft_printf("stack A\n--------------\n");
-	ft_display_list(stack_a);
-	ft_printf("stack B\n--------------\n");
-	ft_display_list(stack_b);
-
 	ft_free_stack(&stack_a);
 	ft_free_stack(&stack_b);
 	return (0);
-
 }

64adc76f54ff5033b570a4623aa1d1198e958d72
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-30 14:52:05 +0100

    Update sorting.h

--- include/sorting.h
+++ include/sorting.h
@@ -15,11 +15,17 @@
 
 # include "push_swap.h"
 
+bool	ft_is_sorted(t_stack *stack);
 int		ft_find_index(t_stack *stack, int target);
-void	ft_sort_top_three(t_stack **stack);
-void	ft_shift_partition(t_stack **stack_a, t_stack **stack_b,
+void	ft_sort_two(t_stack **stack);
+void	ft_sort_three(t_stack **stack);
+void	ft_sort_four(t_stack **stack_a, t_stack **stack_b);
+void	ft_sort_five(t_stack **stack_a, t_stack **stack_b);
+int		ft_find_min(t_stack *stack);
+void	ft_push_to_b(t_stack **stack_a, t_stack **stack_b, int value);
+void	ft_partition_and_shift(t_stack **stack_a, t_stack **stack_b,
 			int pv1, int pv2);
 void	ft_divide_and_shift(t_stack **stack_a, t_stack **stack_b);
-void	ft_shift_b_to_a(t_stack **stack_a, t_stack **stack_b);
-bool	ft_is_sorted(t_stack *stack);
+void	ft_move_element_to_a(t_stack **stack_a, t_stack **stack_b);
+
 #endif

63bc0c6b52720523fabbaddb2af59903f174994c
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-31 14:33:41 +0100

    Update parsing.h

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 19:34:04 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/10/29 12:31:34 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:21:00 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ bool	ft_is_int_range(const char *str);
 bool	ft_has_duplicates(t_stack *stack, int value);
 void	ft_add_to_stack(char *token, t_stack **stack_a);
 void	ft_handle_token(char *arg, t_stack **stack_a);
+bool	ft_is_whitespace(char c);
+bool	ft_str_is_whitespace(const char *str);
 #endif

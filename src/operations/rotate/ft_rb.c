3e5ceee5c41ca9fd56fe53ee0e95de582fcb97ef
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-10-31 15:09:17 +0100

    add protection to the function

--- src/operations/rotate/ft_rb.c
+++ src/operations/rotate/ft_rb.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 10:00:42 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/10/29 10:26:48 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:20:28 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_rb(t_stack **stack_b)
 {
+	if (*stack_b && ft_stack_size(*stack_b) < 2)
+		return ;
 	ft_rotate(stack_b);
 	ft_printf("%s", "rb\n");
 }

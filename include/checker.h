1cfc9213620fa72d81e87b7c998e76c227346e0f
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-11-02 19:54:32 +0100

    Update checker.h

--- include/checker.h
+++ include/checker.h
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:28:43 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/11/02 17:30:02 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/11/02 19:34:25 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,17 @@
 
 # include "push_swap.h"
 # include "get_next_line.h"
+# include "sorting.h"
+# include "operations.h"
+# include "parsing.h"
 
 void	execute_swap_instructions(char *instruction,
 			t_stack **stack_a, t_stack **stack_b);
 void	execute_push_instructions(char *instruction,
 			t_stack **stack_a, t_stack **stack_b);
 void	execute_rotate_instructions(char *instruction,
+			t_stack **stack_a, t_stack **stack_b);
+void	execute_reverse_instructions(char *instruction,
 			t_stack **stack_a, t_stack **stack_b);
 void	execute_instruction(char *instruction,
 			t_stack **stack_a, t_stack **stack_b);

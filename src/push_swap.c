53518e828a378a772fcf809604b6a0021310aec9
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-11-03 02:12:34 +0100

    Update push_swap.c

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 21:11:09 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/11/02 16:05:47 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/11/03 00:36:11 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv)
 	stack_a = NULL;
 	stack_b = NULL;
 	if (argc < 2)
-		exit(1);
+		exit(0);
 	ft_parse_args(argc, argv, &stack_a);
 	if (!ft_is_sorted(stack_a))
 		ft_sort(&stack_a, &stack_b);
